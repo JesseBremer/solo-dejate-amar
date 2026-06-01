52e7b7d676417802f790ccd6ff0b4ff8da8410a7
Jesse Bremer <jessesrbremer@gmail.com>
2026-06-01 15:36:46 -0400

    PushCode

--- netlify/functions/send-push.js
+++ netlify/functions/send-push.js
@@ -62,7 +62,7 @@ exports.handler = async (event) => {
   );
 
   const supabase = createClient(
-    process.env.SUPABASE_URL,
+    'https://dgxxntdgtkiwztlptsdv.supabase.co',
     process.env.SUPABASE_SERVICE_ROLE_KEY,
   );
 
